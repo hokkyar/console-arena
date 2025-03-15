6d8bcc8b5b0a8e089c0e8c9f42a4dd7affb5ee65
Hokky Aryasta <81088460+hokkyar@users.noreply.github.com>
2025-03-15 22:26:04 +0800

    Add css vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', config('app.name'))</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     @yield('content')
-    @vite(['resources/js/app.js'])
     @stack('scripts')
 </body>
 </html>
